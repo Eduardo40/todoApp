133275d6b24b3834e134bd793e5b2a38b16ca09b
eduardo <eduardo1@net.hr>
2017-10-16 00:10:19 +0200

    final refractor, UI improvnets, add custom fonts

--- public/js/myLittleLib.js
+++ public/js/myLittleLib.js
@@ -1,95 +1,98 @@
 'use strict';
-(function(global){
+(function (global) {
     let lib = {};
-    lib.sendRequest = function(method,url,toSend) {
-        return new Promise((res,rej)=>{
+    lib.sendRequest = function (method, url, toSend) {
+        return new Promise((res, rej) => {
             let xhr = new XMLHttpRequest();
-            xhr.open(method,url,true);
+            xhr.open(method, url, true);
             //Check if there is anyting to send (POST)
-            if(toSend){
+            if (toSend) {
                 xhr.setRequestHeader("Content-type", "application/json");
             }
-                xhr.onload = function(data){
-                    if(data.target.status === 200){
-                        res(JSON.parse(data.target.response));
-                    }
-                    rej({status: data.target.status, message:"Something went wrong, please try again"});
+            xhr.onload = function (data) {
+                if (data.target.status === 200) {
+                    res(JSON.parse(data.target.response));
                 }
+                rej({
+                    status: data.target.status,
+                    message: "Something went wrong, please try again"
+                });
+            }
             toSend ? xhr.send(toSend) : xhr.send();
         });
     }
-    lib.displayTodos =  function() {
-        this.sendRequest("GET","http://localhost:3000/api/todos")
-        .then((data)=>{
-            let html = "";
-            data.forEach(function(todo) {
-                if(todo.completed){
-                    html += `
+    lib.displayTodos = function () {
+        this.sendRequest("GET", "http://localhost:3000/api/todos")
+            .then((data) => {
+                let html = "";
+                data.forEach(function (todo) {
+                    if (todo.completed) {
+                        html += `
                     <li class="list-group-item crossed">
                         ${todo.name} 
                         <button class="btn btn-danger btn-sm float-right" data-id="${todo._id}">Delete</button> 
-                    </li>`;    
-                }else{
-                    html += `
+                    </li>`;
+                    } else {
+                        html += `
                     <li class="list-group-item">
                         ${todo.name} 
                         <button class="btn btn-danger btn-sm float-right" data-id="${todo._id}">Delete</button> 
                         <button class="btn btn-success btn-sm float-right" id="btnComplete" data-id="${todo._id}">Complete</button>
                     </li>`;
-                }
-            });
-            document.querySelector("#todoDisplay").innerHTML = html;
-        }).catch((err)=>{
-            throw err;
+                    }
+                });
+                document.querySelector("#todoDisplay").innerHTML = html;
+            }).catch((err) => {
+                throw err;
+            })
+    }
+
+    lib.addTodo = function (todoName, url) {
+        return new Promise((resolve, reject) => {
+            this.sendRequest("POST", url, todoName)
+                .then(response => {
+                    resolve(response);
+                }).catch(err => {
+                    reject(err);
+                })
         })
     }
 
-    lib.addTodo = function (todoName,url){
-        return new Promise((resolve,reject)=>{
-            this.sendRequest("POST",url,todoName)
-            .then(response =>{
-                resolve(response);
-            }).catch(err =>{
-                reject(err);
-            })
-        })
-    }
-
-    lib.deleteTodo = function(todo_id){
-        return new Promise((resolve,reject)=>{
-            this.sendRequest("DELETE",`http://localhost:3000/api/todos/${todo_id}`)
-            .then(successMessage => {
-                resolve(successMessage.message);
-            }).catch(err=> {
-                console.log(err.message);
-            })
+    lib.deleteTodo = function (todo_id) {
+        return new Promise((resolve, reject) => {
+            this.sendRequest("DELETE", `http://localhost:3000/api/todos/${todo_id}`)
+                .then(successMessage => {
+                    resolve(successMessage.message);
+                }).catch(err => {
+                    console.log(err.message);
+                })
         });
     }
 
-    lib.completeTodo = function(todo_id){
-        return new Promise((resolve, reject)=>{
-            this.sendRequest("PUT",`http://localhost:3000/api/todos/${todo_id}`)
-            .then(successMessage => {
-                resolve(successMessage);
-            }).catch(err => {
-                reject(err);
-            });
+    lib.completeTodo = function (todo_id) {
+        return new Promise((resolve, reject) => {
+            this.sendRequest("PUT", `http://localhost:3000/api/todos/${todo_id}`)
+                .then(successMessage => {
+                    resolve(successMessage);
+                }).catch(err => {
+                    reject(err);
+                });
         });
     }
 
-    lib.showMessage = function(messageText,cssClass){
+    lib.showMessage = function (messageText, cssClass) {
         let alertPlace = document.querySelector("#alertMessagePlace");
         alertPlace.innerHTML = "";
-        let html  = `
+        let html = `
         <div class="alert alert-${cssClass}" role="alert">
             ${messageText}
         </div>
         `;
         alertPlace.innerHTML += html;
         let alert = document.querySelector(".alert");
-        setTimeout(function(){
+        setTimeout(function () {
             alert.style.display = "none";
-        },3000)
+        }, 3000)
     }
 
     global.helpers = lib;
--- public/js/script.js
+++ public/js/script.js
@@ -1,58 +1,60 @@
 'use strict';
-(function(popularLib, global,lib){
+(function (popularLib, global, my$) {
 
     //Calling this function because we want to see current todos when the page is loaded.
     //I know there is a difrrent way to do this, but this way is easier ;)
     //Meow
-    lib.displayTodos();
+    my$.displayTodos();
 
-//Start of event listener(s)
-document.querySelector("#form").addEventListener("submit",function(e){
-    e.preventDefault();
-    //stop browser from refreshing on submit
-    const todoName = e.target["0"].value;
-    if(todoName === ""){
-        return lib.showMessage("Please fill out empty field","danger");
-    }
-    const todoNameJson = JSON.stringify({name: todoName});
+    //Start of event listener(s)
+    document.querySelector("#form").addEventListener("submit", function (e) {
+        e.preventDefault();
+        //stop browser from refreshing on submit
+        const todoName = e.target["0"].value;
+        if (todoName === "") {
+            return my$.showMessage("Please fill out empty field", "danger");
+        }
+        const todoNameJson = JSON.stringify({
+            name: todoName
+        });
 
-    lib.addTodo(todoNameJson,"http://localhost:3000/api/todos")
-    .then(response =>{
-        lib.displayTodos();
-        lib.showMessage("New todo added!","success");
-        e.target["0"].value = "";
-    }).catch(err=>{
-        lib.showMessage(err.message,"danger");
+        my$.addTodo(todoNameJson, "http://localhost:3000/api/todos")
+            .then(response => {
+                my$.displayTodos();
+                my$.showMessage("New todo added!", "success");
+                e.target["0"].value = "";
+            }).catch(err => {
+                my$.showMessage(err.message, "danger");
+            })
+
+    });
+
+    //delegate the click event for the delete buttons
+    document.querySelector("body").addEventListener("click", function (e) {
+        if (e.target.classList[1] === "btn-danger") {
+            const id = e.target.dataset.id;
+            my$.deleteTodo(id)
+                .then(message => {
+                    my$.showMessage(message, "success");
+                    my$.displayTodos()
+                })
+                .catch(err => {
+                    my$.showMessage(err.message, "danger");
+                })
+        }
     })
-
-});
-
-//delegate the click event for the delete buttons event
-document.querySelector("body").addEventListener("click",function(e){
-    if(e.target.classList[1] === "btn-danger"){
-        const id = e.target.dataset.id;
-        lib.deleteTodo(id)
-        .then(message =>{
-            lib.showMessage(message,"success");
-            lib.displayTodos()
-        })
-        .catch(err=>{
-            lib.showMessage(err.message,"danger");
-        })
-    }
-})
-
-document.querySelector("body").addEventListener("click",function(e){
-    if(e.target.classList[1] === "btn-success"){
-        const id = e.target.dataset.id;
-        lib.completeTodo(id)
-        .then(response =>{
-            lib.showMessage(response.message,"success");
-            lib.displayTodos()
-        })
-        .catch(err=>{
-            lib.showMessage(err.message,"danger");
-        })
-    }
-})
-}(null,window,helpers));
+    //delegate the click event for the complete buttons
+    document.querySelector("body").addEventListener("click", function (e) {
+        if (e.target.classList[1] === "btn-success") {
+            const id = e.target.dataset.id;
+            my$.completeTodo(id)
+                .then(response => {
+                    my$.showMessage(response.message, "success");
+                    my$.displayTodos()
+                })
+                .catch(err => {
+                    my$.showMessage(err.message, "danger");
+                })
+        }
+    })
+}(null, window, helpers));
